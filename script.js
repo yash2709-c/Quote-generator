46beb01b1fabe2aeed5e48749572caeffcbaefde
Yash Joon <yashj@DESKTOP-IBBUCMI>
2022-01-07 02:39:26 +0530

    complete project

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
-console.log("helloww");
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+let apiQuotes = [];
+
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// hide Loading
+function completer() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+// get quotes from API
+function newQuote() {
+  loading();
+
+  // to pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  if (!quote.author) {
+    authorText.innerText = "Unknown";
+  } else {
+    authorText.innerText = quote.author;
+  }
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.innerText = quote.text;
+  completer();
+}
+
+async function getQuotes() {
+  loading();
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+
+    newQuote();
+  } catch (error) {
+    alert(error);
+    // catch here
+  }
+}
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank"); //'_blank' opens the new tab
+}
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", (y = tweetQuote));
+
+// onload
+getQuotes();
+// completer();
